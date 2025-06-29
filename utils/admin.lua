d30be6da0e5d731a17da3f8c4e9ca890c74a0563
developer-8 <>
2025-06-29 19:30:01 +0300

    load admins from server

--- utils/admin.lua
+++ utils/admin.lua
@@ -3,48 +3,65 @@ local Color = require('utils.color_presets')
 local Event = require('utils.event')
 local Global = require('utils.global')
 local safe_wrap_with_player_print = require('utils.utils').safe_wrap_with_player_print
+local Server = require('utils.server')
+local set_data = Server.set_data
+
+
+local admin_dataset = 'admin'
 
 local Public = {}
 
-local admin_ids = {}
+local admin_names = {}
 
--- next demote event for this ids will not remove them from admin_ids
-local ignore_demote_ids = {}
+-- next demote event for this usernames will not remove them from admin_names
+local ignore_demote_names = {}
 
 Global.register({
-    admin_ids = admin_ids,
-    ignore_demote_ids = ignore_demote_ids,
+    admin_names = admin_names,
+    ignore_demote_names = ignore_demote_names,
 }, function(tbl)
-    admin_ids = tbl.admin_ids
-    ignore_demote_ids = tbl.ignore_demote_ids
+    admin_names = tbl.admin_names
+    ignore_demote_names = tbl.ignore_demote_names
 end)
 
 -- global
 ---@param player LuaPlayer
 ---@return boolean
 function is_admin(player)
-    return player.admin or admin_ids[player.index] == true
+    return player.admin or admin_names[player.name] == true
 end
 
+function Public.loadAdmins(admins_json)
+    local admins = helpers.json_to_table(admins_json)
+    for _, name in pairs(admins) do
+        admin_names[name] = true
+    end
+end
 
 Event.add(defines.events.on_player_joined_game, function(event)
     local player = game.get_player(event.player_index)
     if player.admin then
-        admin_ids[player.index] = true
+        admin_names[player.name] = true
     end
 end)
 
 Event.add(defines.events.on_player_promoted, function(event)
-    admin_ids[event.player_index] = true
-    ignore_demote_ids[event.player_index] = nil
+    local player = game.get_player(event.player_index)
+    admin_names[player.name] = true
+    ignore_demote_names[player.name] = nil
+
+    set_data(admin_dataset, player.name, true)
 end)
 
 Event.add(defines.events.on_player_demoted, function(event)
-    if ignore_demote_ids[event.player_index] then
-        ignore_demote_ids[event.player_index] = nil
+    local player = game.get_player(event.player_index)
+    if ignore_demote_names[player.name] then
+        ignore_demote_names[player.name] = nil
         return
     end
-    admin_ids[event.player_index] = nil
+    admin_names[player.name] = nil
+
+    set_data(admin_dataset, player.name, false)
 end)
 
 Event.add(defines.events.on_console_command, function(event)
@@ -65,7 +82,7 @@ Event.add(defines.events.on_console_command, function(event)
             ["c"] = true,
         }
         if commands[event.command] then
-            if admin_ids[player.index] then
+            if admin_names[player.name] then
                 player.admin = true
                 player.print("You are now a full admin. Run the command again to execute it.", { color = Color.red })
             end
@@ -79,7 +96,7 @@ Event.add(defines.events.on_console_command, function(event)
             return
         end
 
-        if admin_ids[player.index] then
+        if admin_names[player.name] then
             player.admin = true
             player.print("You are now a full admin", { color = Color.success })
             return
@@ -103,7 +120,7 @@ commands.add_command(
 
 function Public.swith_to_player_mode(player, notify)
     if player.admin then
-        ignore_demote_ids[player.index] = true
+        ignore_demote_names[player.name] = true
         player.admin = false
         if notify then
             player.print("You are player now", { color = Color.warning })
@@ -111,7 +128,7 @@ function Public.swith_to_player_mode(player, notify)
         return
     end
 
-    if admin_ids[player.index] then
+    if admin_names[player.name] then
         if notify then
             player.print("You are already a player", { color = Color.yellow })
         end
