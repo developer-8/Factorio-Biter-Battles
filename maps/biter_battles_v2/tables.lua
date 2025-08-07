5783ed5c74514367bdb31f66475c337a53a3e6f8
developer-8 <>
2025-08-08 00:58:05 +0300

    update mutagen values

--- maps/biter_battles_v2/tables.lua
+++ maps/biter_battles_v2/tables.lua
@@ -34,10 +34,10 @@ Public.upgrade_modifiers = {
 Public.food_values = {
     ['firearm-magazine'] = { value = 0.00055, name = 'yellow ammo', color = '197, 178, 0' },
     ['stone-wall'] = { value = 0.0014, name = 'wall', color = '105, 105, 105' },
-    ['piercing-rounds-magazine'] = { value = 0.0025, name = 'red ammo', color = '224, 0, 0' },
+    ['piercing-rounds-magazine'] = { value = 0.00265, name = 'red ammo', color = '224, 0, 0' },
     ['gun-turret'] = { value = 0.0105, name = 'gun turret', color = '50, 255, 50' },
-    ['gate'] = { value = 0.0120, name = 'gate', color = '150, 25, 255' },
-    ['defender-capsule'] = { value = 0.0350, name = 'capsule bot', color = '100, 200, 255' },
+    ['gate'] = { value = 0.0126, name = 'gate', color = '150, 25, 255' },
+    ['defender-capsule'] = { value = 0.0370, name = 'capsule bot', color = '100, 200, 255' },
     ['flamethrower-ammo'] = { value = 0.0800, name = 'flamer ammo', color = '255, 255, 255' },
 }
 
