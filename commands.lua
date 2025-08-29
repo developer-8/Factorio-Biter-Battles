f99b3e3150f58e2c40862d80aae78524e2a359b4
developer-8 <>
2025-08-29 22:44:33 +0300

    follow2 command

--- commands.lua
+++ commands.lua
@@ -1,5 +1,6 @@
 require('commands.chat_commands')
 require('commands.difficulty_voting_commands')
+require('commands.follow2')
 require('commands.instant_map_reset')
 require('commands.inventory_costs')
 require('commands.current_map_seed')
